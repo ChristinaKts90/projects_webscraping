59151c17dcf40d326dfe0c0fd06836b39a528497
Christinadidi10 <52113095+Christinadidi10@users.noreply.github.com>
2020-05-22 19:31:14 +0100

    new

--- quotes_scrape/quotes_scrape/spiders/quotes.py
+++ quotes_scrape/quotes_scrape/spiders/quotes.py
@@ -1,11 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com/js']
-    start_urls = ['http://quotes.toscrape.com/js/']
+
+    script = '''
+
+        function main(splash, args)
+        splash.private_mode_enabled = false
+        url = args.url
+        assert(splash:go(url))
+        assert(splash:wait(0.5))
+        return {
+            html = splash:html(),
+        }
+        end
+
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url='http://quotes.toscrape.com/js/', callback=self.parse, endpoint="execute", args = {
+            'lua_source': self.script
+        })
+
+
+
+
 
     def parse(self, response):
-        pass
+        #print(response.body)
+        for quote in response.xpath("//div[@class='quote']"):
+            yield{
+                'Quote Text': quote.xpath(".//span[@class='text']/text()").get(),
+                'Author': quote.xpath(".//span/small[@class='author']/text()").get(),
+                'Tags': quote.xpath(".//div[@class='tags']/a/text()").getall()
+
+            }
